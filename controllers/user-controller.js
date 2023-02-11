72b6f371eaddb4add326d2ea0bc5168bf2657de7
Chen ChiaChi <chenchiachi.ccc@gmail.com>
2023-02-11 01:22:47 +0000

    Add signUp api

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,20 +1,44 @@
-const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const { User } = require('../models')
 
 const userController = {
   login: (req, res, next) => {
     try {
-        const userData = req.user.toJSON()
-        delete userData.password //避免密碼傳入前端
-        const token = jwt.sign(userData, process.env.JWT_SECRET, {
-          expiresIn: '30d'
-        })
-        res.json({
-          status: 'success',
-          data: {
-            token,
-            user: userData
-          }
-        })
+      const userData = req.user.toJSON()
+      delete userData.password //避免密碼傳入前端
+      const token = jwt.sign(userData, process.env.JWT_SECRET, {
+        expiresIn: '30d'
+      })
+      res.json({
+        status: 'success',
+        data: {
+          token,
+          user: userData
+        }
+      })
+    } catch (err) {
+      next(err)
+    }
+  },
+  signUp: async (req, res, next) => {
+    try {
+      const { account, name, email, password, checkPassword } = req.body
+      if (!account || !name || !email || !password || !checkPassword) throw Error('All fields are require.')
+      if (/\s/.test(account) || /\s/.test(password)) throw Error('Not allowed space.')
+      if (name.length > 50) throw Error('Name is too long!')
+      if (password !== checkPassword) throw Error('Passwords do not match!')
+      const userEmail = await User.findOne({ where: { email } })
+      const userAccount = await User.findOne({ where: { account } })
+      if (userEmail) throw Error('Email already existed!')
+      if (userAccount) throw Error('Account already existed!')
+      await User.create({
+        account,
+        name,
+        email,
+        password: bcrypt.hashSync(password, bcrypt.genSaltSync(10), null)
+      })
+      return res.json({ status: 'SignUp success.' })
     } catch (err) {
       next(err)
     }
--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,17 @@ const router = express.Router()
 const passport = require('../config/passport')
 
 const admin = require('./modules/admin')
-
+const user = require('./modules/user')
 const userController = require('../controllers/user-controller')
 const { authenticated, authenticatedAdmin } = require('../middleware/api-auth')
 const { generalErrorHandler } = require('../middleware/errHandler')
 
+router.post('/login', passport.authenticate('local', { session: false }), userController.login)
+router.post('/users', userController.signUp)
+
+router.use('/users', authenticated, user)
 router.use('/admin', authenticated, authenticatedAdmin, admin)
 
-router.post('/login', passport.authenticate('local', { session: false }), userController.login)
 
 router.use('/', generalErrorHandler)
 module.exports = router
